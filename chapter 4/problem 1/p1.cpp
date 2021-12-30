9dcdbd96732ac16d4d61b2bcb3d94cf49cd9abb9
ismail-hossain-riyad <dominantriyad@gmail.com>
2021-12-31 04:10:49 +0600

    4.1

---
+++ chapter 4/problem 1/p1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //---------------BASIC FOR LOOP STRUCTURE-------------
+ /*PRINTING BANGLADESH WITH NUMBER
+ for(int INITIALIZATION;CONDITION;INCREAMENT/DECREAMENT )
+ {
+   STATEMENT
+ }   */
+
+
+//  for( ;; )
+// {
+//   cout<<"ISMAIL"<<endl;
+// }
+
+
+
+
+  for( int i=1;i<=5;i++ )
+ {
+   cout<<"bangladesh"<<endl;
+ }
+
+return 0;
+}
