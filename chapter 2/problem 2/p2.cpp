48be8650259163cb6fab22d59e3fa1377e2623af
ismail-hossain-riyad <dominantriyad@gmail.com>
2021-12-31 01:24:16 +0600

    2.2

---
+++ chapter 2/problem 2/p2.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+ int i=5;//decralation for postfix increament
+
+//---------------------------POSTFIX INCREAMENT--------------------------------------
+
+
+  //1
+  cout<<" Before increament = "<<i<<endl;
+  //2
+  cout<<"postfix increament="<<i++<<endl;
+  //3
+  cout<<"after increament ="<<i<<endl;
+
+   int j=9;//decralation for prefix increament
+
+//---------------------------PREFIX INCREAMENT--------------------------------------
+
+
+  //1
+  cout<<" Before increament = "<<j<<endl;
+  //2
+  cout<<"prefix increament="<<++j<<endl;
+  //3
+  cout<<"after increament ="<<j<<endl;
+
+
+
+
+
+
+
+
+
+
+  int m=5;//decralation for postfix decreament
+
+//---------------------------POSTFIX decreament--------------------------------------
+
+
+  //1
+  cout<<" Before decreament = "<<m<<endl;
+  //2
+  cout<<"postfix decreament="<<m--<<endl;
+  //3
+  cout<<"after decreament ="<<m<<endl;
+
+   int n=9;//decralation for prefix decreament
+
+//---------------------------PREFIX decreament--------------------------------------
+
+
+  //1
+  cout<<" Before decreament = "<<n<<endl;
+  //2
+  cout<<"prefix decreament="<<--n<<endl;
+  //3
+  cout<<"after decreament ="<<n<<endl;
+
+}
+
