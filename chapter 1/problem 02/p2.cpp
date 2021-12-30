8511668f5d398d9dba6c8ff288a6a41b53ace517
ismail-hossain-riyad <dominantriyad@gmail.com>
2021-12-30 21:14:54 +0600

    1.2

---
+++ chapter 1/problem 02/p2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+short a1=225;
+int a2=256;
+long a3=47458369846;
+long long a4 =734643589548;
+float f1=3.99;
+double f2=3.121416;
+char c1='s';
+bool b1=true;
+cout<<"value of a1 is ="<<a1<<endl;
+cout<<a2<<endl;
+cout<<a3<<endl;
+cout<<a4<<endl;
+cout<<f1<<endl;
+cout<<f2<<endl;
+cout<<c1<<endl;
+cout<<b1<<endl;
+
+}
