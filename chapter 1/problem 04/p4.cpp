cfba9d57ba8546e7812aa2b517c86d1c558a4002
ismail-hossain-riyad <dominantriyad@gmail.com>
2021-12-30 23:04:13 +0600

    1.4

---
+++ chapter 1/problem 04/p4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//int b,h,k;
+//cin>>b>>h;
+//k=0.5*b*h;
+//cout<<"The area of Triangle is "<<k<< endl;
+//float pi=3.1416;
+//int r;
+//float k;
+//cin>>r;
+//k=pi*r*r;
+//cout<<"The are of Circle "<<k<<endl;
+//float pi=3.1416,c;
+//int r;
+//cin>>r;
+//c=2*pi*r;
+//cout<<"Perimeter of Circle is "<<c<<endl;
+
+}
