fafdf9a63c05fa7cd2a3b1c2c3a13096f9265821
ismail-hossain-riyad <dominantriyad@gmail.com>
2021-12-31 00:17:02 +0600

    2.1

---
+++ chapter 2/problem 1/p1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//  int year;
+//  cin>>year>>endl;
+//  i) year%400==0
+//  ii) year%4==0&&year%100!=0
+
+int i=9;
+
+while(i>0)
+
+{cout<<"ismail"<<endl;
+i++;
+}
+}
