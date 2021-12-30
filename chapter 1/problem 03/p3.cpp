b71ab08526f7e77137fb7f25645a2c5b5122ec56
ismail-hossain-riyad <dominantriyad@gmail.com>
2021-12-30 21:53:42 +0600

    1.3

---
+++ chapter 1/problem 03/p3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=19,b=05,sum=0,multiplication=1;
+  sum=a+b;
+  multiplication=a*b;
+  cout<<"The sum is :"<<sum<<endl;
+  cout<<"the multiplication="<<multiplication<<endl;
+     return 0;
+}
